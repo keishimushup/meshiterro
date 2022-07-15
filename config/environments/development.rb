d789a779b3e39e17393ea18a34a751c9028138c0
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-15 12:02:00 +0000

    [Finish]4-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
   
   config.hosts << "bcc5e65171b640078bef01e15980ea74.vfs.cloud9.us-east-1.amazonaws.com"
   
+  config.active_job.queue_adapter = :inline
+  
 end
