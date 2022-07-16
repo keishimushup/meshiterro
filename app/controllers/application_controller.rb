3c3329049a11113ccfaafd54a6c1e29d2047e697
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-16 04:10:22 +0000

    [Finish]4-6 新規登録ログインログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramenters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_paramenters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
